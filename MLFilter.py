ad207074911cfb33a9833dda786e62e82efd6202
SPier <spierrett@gmail.com>
2025-07-22 01:32:50 -0400

    init

---
+++ MLFilter.py
@@ -0,0 +1,76 @@
+#from sklearn import svm
+import pandas as pd
+import numpy as np
+from ucimlrepo import fetch_ucirepo
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.ensemble import RandomForestClassifier
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+tuneHyperPar=False #if to tune the hyperparameters of the classifiers
+
+def viewDataStats(data):
+    if (isinstance(data, pd.DataFrame)):
+        total_nan = data.isna().sum().sum()
+        print("Data Dimensions : %d rows, %d columns" % (data.shape[0], data.shape[1]))
+        print("Total Non-Numeric Values : %d " % (total_nan))
+        print("Name", "Type", "#Distinct", "NAN Values")
+        columns = data.columns
+        types = data.dtypes
+        unique = data.nunique()
+        nan_values = data.isna().sum()
+        for i in range(len(data.columns)):
+            print(columns[i], types[i], unique[i], nan_values[i])
+        
+    else:
+        print("Not a Dataframe" + str(type(data)))
+        exit(0)
+
+def showImportantFeatures(model,cols): #for tree-based models
+    importances = model.feature_importances_
+    sns.barplot(x=importances, y=cols[:-1])
+    plt.show()
+def showCorMatrix(spamData):
+    spamData=spamData.iloc[:, : -1]
+    correlation_matrix = spamData.corr()
+    sns.heatmap(correlation_matrix, cmap='coolwarm', annot=False)
+    plt.show()
+
+if __name__=="__main__":
+    spambase_data = pd.read_csv('spambase.csv', header=None)
+
+    spambase_data.columns=spambase_data.iloc[0] # copy column names
+    
+    data = spambase_data.drop(index=0) #remove row of columns names
+    data.reset_index(drop=True, inplace=True)
+    
+    showCorMatrix(data)
+    
+
+    X = data.iloc[:, : -1].values #separate first n-1 column data which are features
+
+    y = data.iloc[:,  -1].values  #separate last n column data is which is a target column
+
+    
+
+    X_train, X_test, y_train, y_test =train_test_split(X, y, test_size=0.3, random_state=99)
+
+    clf=None
+    #clf = MultinomialNB() #Bayes
+    if (not tuneHyperPar):
+        clf = RandomForestClassifier(random_state=42) #Random Forest
+        clf.fit(X_train, y_train)
+        showImportantFeatures(clf,spambase_data.columns)
+    else:
+        from sklearn.model_selection import GridSearchCV
+     
+        param_grid = {'n_estimators': [50, 100, 200], 'max_depth': [None, 10, 20]}
+        clf = GridSearchCV(RandomForestClassifier(random_state=42), param_grid, cv=5)
+        clf.fit(X_train, y_train)
+         
+    print("Accuracy:"+(str)(clf.score(X_test, y_test)))
+    
+
+
+    
---
+++ MLFilter_pyspark.py
@@ -0,0 +1,93 @@
+from pyspark.ml import Pipeline
+import pandas as pd
+import numpy as np
+from ucimlrepo import fetch_ucirepo
+from pyspark.ml.classification import NaiveBayes
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+from pyspark.ml.classification import RandomForestClassifier
+import matplotlib.pyplot as plt
+import seaborn as sns
+from pyspark import sql
+from pyspark.sql import SparkSession,types
+from pyspark.ml.feature import VectorAssembler
+
+tuneHyperPar=False #if to tune the hyperparameters of the classifiers
+
+def viewDataStats(data):
+    if (isinstance(data, pd.DataFrame)):
+        total_nan = data.isna().sum().sum()
+        print("Data Dimensions : %d rows, %d columns" % (data.shape[0], data.shape[1]))
+        print("Total Non-Numeric Values : %d " % (total_nan))
+        print("Name", "Type", "#Distinct", "NAN Values")
+        columns = data.columns
+        types = data.dtypes
+        unique = data.nunique()
+        nan_values = data.isna().sum()
+        for i in range(len(data.columns)):
+            print(columns[i], types[i], unique[i], nan_values[i])
+        
+    else:
+        print("Not a Dataframe" + str(type(data)))
+        exit(0)
+
+def showImportantFeatures(model,cols): #for tree-based models
+    importances = model.feature_importances_
+    sns.barplot(x=importances, y=cols[:-1])
+    plt.show()
+def showCorMatrix(spamData):
+    spamData=spamData.iloc[:, : -1]
+    correlation_matrix = spamData.corr()
+    sns.heatmap(correlation_matrix, cmap='coolwarm', annot=False)
+    plt.show()
+
+if __name__=="__main__":
+    spambase_data = pd.read_csv('spambase.csv', header=None)
+
+    spambase_data.columns=spambase_data.iloc[0] # copy column names
+    
+    data = spambase_data.drop(index=0) #remove row of columns names
+    data.reset_index(drop=True, inplace=True)
+    
+    showCorMatrix(data)
+    data = data.rename(columns={'Spam': 'label'})
+    spark = SparkSession.builder.appName('example').getOrCreate()
+    data=spark.createDataFrame(data)
+    feature_list = []
+    for col in data.columns:
+        if col == 'label' or col == 'Label':
+            continue
+        else:
+            feature_list.append(col)
+    for col in data.columns:
+        data = data.withColumn(col, data[col].cast(sql.types.FloatType()))
+    #data = data.withColumnRenamed("Spam", "label")
+    train_data, test_data = data.randomSplit([0.7, 0.3], seed=42)
+    cv_model=None
+    assembler = VectorAssembler(inputCols=feature_list, outputCol="features")
+    clf = NaiveBayes(smoothing=1.0, modelType="multinomial")
+    #clf = RandomForestRegressor(labelCol="label", featuresCol="features")    
+    pipeline=Pipeline(stages=[assembler,clf])
+    #clf = MultinomialNB() #Bayes
+    if (not tuneHyperPar):
+        cv_model = pipeline.fit(train_data)
+    else:
+        paramGrid = ParamGridBuilder() \
+        .addGrid(rf.numTrees, [int(x) for x in np.linspace(start = 10, stop = 50, num = 3)]) \
+        .addGrid(rf.maxDepth, [int(x) for x in np.linspace(start = 5, stop = 25, num = 3)]) \
+        .build()
+
+        cross_validator = CrossValidator(estimator=pipeline,
+                              estimatorParamMaps=paramGrid,
+                              evaluator=MulticlassClassificationEvaluator(labelCol="label", metricName="accuracy"),
+                              numFolds=5, seed=42)
+        cv_model = cross_validator.fit(train_data)
+         
+    preds = cv_model.transform(test_data)
+    evaluator = MulticlassClassificationEvaluator(labelCol="label", metricName="accuracy")
+
+    accuracy = evaluator.evaluate(preds)
+    print("Test set accuracy ="+str(accuracy))
+    
+
+
+    
